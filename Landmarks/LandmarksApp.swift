f19183474400bff76ed5acdeda11a5164eaa6fe2
Egor Pogadaev <egor.pogadaev@solveva.com>
2022-10-09 21:29:47 +0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Egor Pogadaev on 09.10.22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
